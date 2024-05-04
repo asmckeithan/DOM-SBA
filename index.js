1cb16f9b51a4e6b73c3928b52a9040c2f2e9d96f
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-03 22:43:02 -0400

    added box and styling to Form

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(`Game Styling and code `)
+
+const header = document.getElementsByTagName('h1')
+
+console.log(`Form Styles and Validation` )
+// first we find the form 
+const form = document.getElementById('game')
+console.log(form)
+//next syle the form 
+form.style = ("box-shadow: 3px 3px 2px #d1397a")

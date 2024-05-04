72811a20ea064bc9c973abbd70d09e34ad6006eb
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-03 23:29:08 -0400

    adjusted color and style

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log(`Game Styling and code `)
-
+//adding 
 const header = document.getElementsByTagName('h1')
+console.log(header)
+
+
+
 
 console.log(`Form Styles and Validation` )
 // first we find the form 
@@ -8,3 +12,4 @@ const form = document.getElementById('game')
 console.log(form)
 //next syle the form 
 form.style = ("box-shadow: 3px 3px 2px #d1397a")
+form.style.marginTop = "100px"

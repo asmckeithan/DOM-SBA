15c7cea9f4922c0780b4e5b067b1c4110fc94339
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-05 13:01:45 -0400

    added Div Container for game

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ console.log(header)
 
 console.log(`Form Styles and Validation` )
 // first we find the form 
-const form = document.getElementById('game')
+const form = document.getElementById('form');
 console.log(form)
 //next syle the form 
-form.style = ("box-shadow: 3px 3px 2px #d1397a")
-form.style.marginTop = "100px"
+form.style = ("box-shadow: 4px 4px 4px #0c3170");
+
+
+console.log("Creating a Div Element with style ")
+const div = document.createElement('div');
+form.prepend (div);
+div.style.display = 'inline-block';
+div.style.width = '200px';
+div.textContent = "Sign-up \n to continue Playing" ;
+div.style.fontSize = '20px';
+div.style.padding = '20px'
+div.style.marginTop = '4px'
+console.log(div);
+
